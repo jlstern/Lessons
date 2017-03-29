8409a317f0be7008a32c59cbcacc439ad37ade7d
jlstern <jstern@JStern-EL.local>
2017-03-29 17:23:48 -0400

    End of work 3/29

--- 21_Databases/3_Mongoose.js
+++ 21_Databases/3_Mongoose.js
@@ -54,9 +54,40 @@ Cat.create({
 	}
 });
 
-
-1) install Mongoose
-2) 
+// Steps to add a database
+// 1) install Mongoose
+// 2) require Mongoose
+var mongoose = require("mongoose");
+// 3) set up schema
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+});
+// 4) create a model
+var Campground = mongoose.model("Campground", campgroundSchema);
+// 5) Set up get request to request the data an object
+app.get("/campgrounds", function(req, res){
+Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log("Something went wrong");
+    } else {
+      res.render("campgrounds", {campgrounds:allCampgrounds});
+    }
+  });
+});
+// 6) Set up post request to add to database
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name:name, image:image}
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err){
+      console.log("Something went wrong");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
 
 
 
